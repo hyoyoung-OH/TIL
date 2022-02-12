ec1147f8b2d803d7148122ad928fe95b7cf99453
hyoyoung-OH <lisa4397@gmail.com>
2022-02-13 00:32:39 +0900

    Helloworld출력하기

---
+++ JAVA/jump2java/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+//public: 메소드의 접근제어자, public은 누구나 이 메소드에 접근할 수 있단 읨;
+//static: 메소드에 static이 지정되어 있는 경우 이 메소드는 인스턴스 생성없이 실행할 수 있음을 의미
+//void: 메소드의 리턴값이 없음을 의미
+//String[]: 문자열을 나타내는 자바의 자료형([]가 있으므로 한 개가 아닌 여러개의 겂으로 이루어진 배열임을 의미}
+//args: String[] 자료형에 대한 변수명
+//System.out.println: 표준 출력으로 데이터를 보내는 자바의 내장 메소드로 문자열을 화면에 출력한다.
