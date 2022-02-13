54e415dbb3269dbe87d16236ff862cfc7ffc9014
hyoyoung-OH <lisa4397@gmail.com>
2022-02-13 21:50:43 +0900

    Boolean

--- JAVA/jump2java/src/Sample.java
+++ JAVA/jump2java/src/Sample.java
@@ -1,22 +1,40 @@
 public class Sample {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
+//        //Number
+//        int a = 10;
+//        int b = 5;
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+//
+//        System.out.println(7 % 3);
+//        System.out.println(3 % 7);
+//
+//        int i = 0;
+//        int j = 10;
+//        i++;
+//        j--;
+//
+//        System.out.println(i);
+//        System.out.println(j);
 
-        System.out.println(7 % 3);
-        System.out.println(3 % 7);
+        //boolean
+        boolean isSuccess = true;
+        boolean isTest = false;
 
-        int i = 0;
-        int j = 10;
-        i++;
-        j--;
+        //조건문
+        int base = 180;
+        int height = 185;
+        boolean isTall = height > base;
 
-        System.out.println(i);
-        System.out.println(j);
+        if (isTall) {
+            System.out.println("키가 큽니다");
+        }
+
+        int i = 3;
+        boolean idOdd = i % 2 == 1;
+        System.out.println(idOdd);
 
     }
 }
