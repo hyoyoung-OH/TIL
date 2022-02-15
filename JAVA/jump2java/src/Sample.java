dfc5c9d817dd62c3a238a33c7306a59c6ff3ff06
hyoyoung-OH <lisa4397@gmail.com>
2022-02-15 23:44:43 +0900

    method

--- JAVA/jump2java/src/Sample.java
+++ JAVA/jump2java/src/Sample.java
@@ -1,24 +1,15 @@
+//파라미터: 메소드에 입력으로 전달된 값을 받는 변수
+//arguments(인수): 메소드를 호출할 때 전달하는 입력값
 
-class Animal {
-    String name; //Animal 클래스에 변수를 추가함 => 객체변수
-    //객체변수의값은 공유가 안됨. 독립적으로 유지
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
 
 public class Sample {
+    String say() {
+        return "Hi";
+    }
+
     public static void main(String[] args) {
-        Animal cat = new Animal(); //new는 객체를 생성할 때 사용되는 키워드로 Animal 클래스의 인스턴스인 cat, 즉 Animal의 객체가 만들어짐
-        //객체와 인스턴스
-        // 인스턴스라는 말은 특정 객체가 어떤 클래스의 객체인지를 관계 위주로 설명할 때 사용
-        cat.setName("bobo");
-
-        Animal dog = new Animal();
-        dog.setName("popo");
-
-        System.out.println(cat.name);
-        System.out.println(dog.name);
+        Sample sample = new Sample();
+        String a = sample.say();
+        System.out.println(a);
     }
 }
