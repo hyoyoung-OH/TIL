3bacc2d5fbf5d3c1e74bd574dd571508b263dc20
hyoyoung-OH <lisa4397@gmail.com>
2022-03-04 23:58:44 +0900

    const&let

--- javascript/momentum/app.js
+++ javascript/momentum/app.js
@@ -1,11 +1,13 @@
 const a = 5;
 const b = 2;
 
-const myName = "lisa";
-//변수에 공백을 주고 싶을 때는 카멜케이스로 사용
+let myName = "lisa";
 
 console.log(a * b);
 console.log(a + b);
 console.log(a / b);
 
+console.log("hello " + myName);
+
+myName = "oh";
 console.log("hello " + myName);
---
+++ javascript/momentum/note.md
@@ -0,0 +1,5 @@
+```javascript
+const myName = "lisa";
+```
+
+변수에 공백을 주고 싶을 때는 카멜케이스로 사용한다.
