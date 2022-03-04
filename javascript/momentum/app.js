0ff7144c91f0dca27c75c11b44fce4008f4368ba
hyoyoung-OH <lisa4397@gmail.com>
2022-03-04 23:48:33 +0900

    variables

--- javascript/momentum/app.js
+++ javascript/momentum/app.js
@@ -1,1 +1,11 @@
-alert("hi");
+const a = 5;
+const b = 2;
+
+const myName = "lisa";
+//변수에 공백을 주고 싶을 때는 카멜케이스로 사용
+
+console.log(a * b);
+console.log(a + b);
+console.log(a / b);
+
+console.log("hello " + myName);
