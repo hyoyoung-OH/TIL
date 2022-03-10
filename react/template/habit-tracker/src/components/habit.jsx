5ffbf00ba93cd3095ead5f39a07df2a467576516
hyoyoung-OH <lisa4397@gmail.com>
2022-03-11 01:54:32 +0900

    props 이해하기

--- react/template/habit-tracker/src/components/habit.jsx
+++ react/template/habit-tracker/src/components/habit.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    //state 오브젝트 안에 있는 count를 증가시킨 뒤 state를 업데이트 해야함.
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-increase">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fa-solid fa-square-plus"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i class="fa-solid fa-square-minus"></i>
         </button>
         <button className="habit-button habit-delete">
