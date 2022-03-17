5bd6c4f36976d56c5c7237bcee83901b8904bfe9
hyoyoung-OH <lisa4397@gmail.com>
2022-03-17 15:19:57 +0900

    js추가

--- javascript/browser101/ShoppingList/main.js
+++ javascript/browser101/ShoppingList/main.js
@@ -9,15 +9,21 @@ const addBtn = document.querySelector(".footer__button");
 function onAdd() {
   //1. 사용자가 입력한 텍스트를 받아옴
   const text = input.value;
-  console.log(text);
+  if (text === "") {
+    input.focus();
+    return;
+  }
   //2. 새로운 아이템을 만듬(텍스트 + 삭제버튼)
   const item = createItem(text);
   //3. items 컨테이너안에 새로 만든 아이템을 추가한다
   items.appendChild(item);
-  //4. 인풋을 초기화한다.
+  //4.  새로 추가된 아이템으로 스크롤링
+  item.scrollIntoView({ block: "center" });
+  //5. 인풋을 초기화한다.
   input.value = "";
   input.focus();
 }
+
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
@@ -46,6 +52,13 @@ function createItem(text) {
   itemRow.appendChild(itemDivider);
   return itemRow;
 }
+
 addBtn.addEventListener("click", () => {
   onAdd();
+});
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    onAdd();
+  }
 });
