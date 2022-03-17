794d926211e4806018bb1b207ed61de57cf1d3b3
hyoyoung-OH <lisa4397@gmail.com>
2022-03-18 03:24:10 +0900

    이벤트 위임 - 개선

--- javascript/browser101/ShoppingList/main.js
+++ javascript/browser101/ShoppingList/main.js
@@ -1,3 +1,7 @@
+// 이벤트 위임: 클릭 이벤트를 items부모에만 등록한 다음 쓰레기통 아이템만 클릭되었을 때 해당하는 아이템을 삭제할 것
+// 쓰레기통 아이콘을 클릭했을 떄 해당하는 아이템인 지 아닌 지 어떻게 알까?
+// 아이템마다 고유한 아이디를 부여하고 쓰레기통 아이템에 아이디를 기억하게 해서 아이콘이 클릭되면 아이콘에 할당되어 있는 아이디를 활용해서 원하는 아이템을 찾아서 삭제
+
 // 1. 사용자가 타이핑 할 수 있다.
 // 2. 사용자가 +키나 엔터키를 쳤을 때 item을 등록한다
 // 3. 쓰레기통 아이콘을 클릭하면 아이템이 삭제됨
@@ -24,32 +28,21 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; //UUID를 이용하는 것이 더 좋음
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+      <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete">
+          <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+      </div>
+      <div class="item divider"></div>
+  `;
+  id++;
   return itemRow;
 }
 
@@ -60,5 +53,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
