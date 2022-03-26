a2598df635fb7e98ff998fe7cab168bc497f6356
hyoyoung-OH <lisa4397@gmail.com>
2022-03-27 03:20:42 +0900

    add new meetup 신청양식 추가

--- react/react-udemy-02/src/pages/NewMeetup.js
+++ react/react-udemy-02/src/pages/NewMeetup.js
@@ -1,7 +1,13 @@
 // 자신이 좋아하는 meetup 볼 수 있음
+import NewMeetupForm from '../component/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
