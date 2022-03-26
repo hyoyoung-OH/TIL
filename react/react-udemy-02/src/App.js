125d972adcbe8fe26ffeaa9f706e5bd62aefc1f0
hyoyoung-OH <lisa4397@gmail.com>
2022-03-26 18:18:38 +0900

    라우팅 추가

--- react/react-udemy-02/src/App.js
+++ react/react-udemy-02/src/App.js
@@ -1,21 +1,25 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import AllMeetupsPage from './pages/AllMeetup';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './component/layout/MainNavigation';
 
 function App() {
   return (
     <div>
-      <Route path='/'>
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetupPage />
-      </Route>
-      <Route path='/Favorites'>
-        <FavoritesPage />
-      </Route>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/Favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
