3bc4b4b5e5dec53e5f63aea57689a5d2355bf4a8
hyoyoungOH <lisa4397@gmail.com>
2022-06-21 01:20:20 +0900

    반복문 연습문제 1

--- javascript/zerobase_js/basictest1_condition.js
+++ javascript/zerobase_js/basictest1_condition.js
@@ -2,6 +2,45 @@
 // 입력값: 5, 7, 12
 // 출력값: #1: ***** #2: ******* #3: ************
 
+/* user  code */
+
 function answer(num) {
   let result = '';
+
+  //내가 구현한 코드
+  switch (num) {
+    case 5:
+      result = '*****';
+      break;
+    case 7:
+      result = '*******';
+      break;
+    case 12:
+      result = '************';
+      break;
+  }
+
+  /* 답안
+  for (let i = 0; i < num; i++) {
+    result += '*';
+  }
+  */
+
+  return result;
+}
+
+/*main code */
+let input = [
+  // TC: 1
+  5,
+
+  //TC: 2
+  7,
+
+  //TC: 3
+  12,
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${answer(input[i])}`);
 }
